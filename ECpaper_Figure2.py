7815fbf101735c19e643e2fd4db317231c33dea9
klawa <40496187+klawa@users.noreply.github.com>
2024-06-13 14:22:09 +0200

    Update ECpaper_Figure2.py

--- ECpaper_Figure2.py
+++ ECpaper_Figure2.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This code produces Figure2 from the paper
- Higher increase in dry spell duration unveiled in future climate projections" (ref. below).
+  "Observation-constrained projections reveal longer-than-expected dry spells"
 
 The code is split into independent functions named
 according to the figure subplots numbering in the paper.
@@ -13,7 +13,7 @@ according to the figure subplots numbering in the paper.
 @Contact:      irina.petrova@ugent.be
 ===============================================
 REFERENCE:
-    I.Y. Petrova, Diego G.M., Florent B., Markus G.D., Seung-Ki M., Yeon-Hee K., Margot B. Higher increase in dry spell duration unveiled in future climate projections (2024)
+    Petrova, I.Y., Miralles D.G., ...
 """
 #%%  Import Libraries: 
 # ======================
@@ -41,7 +41,8 @@ def figure_2ab(dat5,dat6, ylabel_nam=None):
     figure and axis objects
     
     '''
-    fig, ax1 = plt.subplots(figsize=(8, 6))
+    fig, ax1 = plt.subplots(figsize=(3, 5),dpi=600)
+    
     # CMIP5:
     # --------------
     x_mod = dat5['x_mod']
@@ -50,7 +51,7 @@ def figure_2ab(dat5,dat6, ylabel_nam=None):
     
     kldiv.EC_regress_confidence_int_func(ax1, dat5['ECSprior'], dat5['ECSpost'], dat5['x_mod'], dat5['y_mod'], dat5['x_obs'],\
                                    np.mean(dat5['x_obs']), dat5['x_obsstd'], dat5['mod_pdf'],\
-                                       modeltype='CMIP5',colm='#8b8b8c',col='#0054ff', msize=200,labelx = 'Historical LAD | 1998-2018 [days]', labely=ylabel_nam, \
+                                       modeltype='CMIP5',colm='#8b8b8c',col='#0054ff', msize=16,labelx = 'Historical LAD | 1998-2018 [days]', labely=ylabel_nam, \
                                            annotate_lable = list(label), makefigure=True, annotate=False, set_title=False, safefig=False, write_file=False)
     # CMIP6:
     # --------------
@@ -60,7 +61,7 @@ def figure_2ab(dat5,dat6, ylabel_nam=None):
     
     kldiv.EC_regress_confidence_int_func(ax1, dat6['ECSprior'], dat6['ECSpost'], dat6['x_mod'], dat6['y_mod'], dat6['x_obs'],\
                                    np.mean(dat6['x_obs']), dat6['x_obsstd'], dat6['mod_pdf'],\
-                                       modeltype='CMIP6',colm='darkred',col='#0054ff', labelx = 'Historical LAD | 1998-2018 [days]', labely=ylabel_nam, \
+                                       modeltype='CMIP6',colm='darkred',col='#0054ff', msize=4,labelx = 'Historical LAD | 1998-2018 [days]', labely=ylabel_nam, \
                                            annotate_lable = list(label), makefigure=True, annotate=False, set_title=False, safefig=False, write_file=False)
                                                                   
     return fig,ax1
@@ -86,6 +87,8 @@ path = input_path+'/project_out_data/'
     
 dat5 = pickle.load(open(path+'EC_global_dataset_RCP8.5_CMIP5_LADchange.pkl','rb'))
 dat6 = pickle.load(open(path+'EC_global_dataset_SSP8.5_CMIP6_LADchange.pkl','rb'))
+#dat5 = pickle.load(open(path+'EC_global_dataset_RCP4.5_CMIP5_LADchange.pkl','rb'))
+#dat6 = pickle.load(open(path+'EC_global_dataset_SSP4.5_CMIP6_LADchange.pkl','rb'))
 
 ## Data is saved in the format:
 '''
@@ -107,6 +110,8 @@ dat5 = OrderedDict([('MODEL_RUN',['CMIP5','LAD change']),
 # Call the plotting function:
 fig,ax = figure_2ab(dat5,dat6, ylabel_nam='Future LAD [days]')
     
+
+
 
 
 
